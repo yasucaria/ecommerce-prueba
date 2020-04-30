b75aa0c66f285fd7e3079855ca4f05127826af33
ysucaria <ya_sucaria@hotmail.com>
2020-04-30 08:11:59 -0300

    html y css

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const detalleProductos = JSON.parse(fs.readFileSync('./site/detalleProductos.json', 'utf-8'));
 
 const productosController = {
-listado:  function(req, res){
-    res.send("Listado de productos");
+listado: function(req, res){
+    res.send(detalleProductos);
 },
 detalle: function(req, res){
     res.send("Este es el detalle de productos");
 }
 }
+
+
 
 module.exports= productosController;

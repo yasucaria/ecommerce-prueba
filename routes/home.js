f696ca84679fe2ac0b2c76c9b7bda6bdc8694b0a
ysucaria <ya_sucaria@hotmail.com>
2020-05-12 08:33:14 -0300

    ruteo

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', function(req, res){
-    res.send("Bienvenidos a Mercado Libro");
-});
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
   });
   
   module.exports = router;
 
 
-module.exports = router
